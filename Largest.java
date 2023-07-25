9b8394cb7b8f19106ae05674c307371c394d7d0b
aditya <adityalavate2000@gmail.com>
2023-07-25 08:24:08 +0530

    first commit

---
+++ Largest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
+
+        int sum = 0;
+        int largest = num1;
+
+        if (num2 > largest) {
+            largest = num2;
+        }
+
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        sum = largest;
+
+        System.out.println("The sum of the largest number is: " + sum);
+    }
+}
